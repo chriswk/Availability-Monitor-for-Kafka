683f03b033038d52f5a9890fa0712d61649ae4f5
ankitsinghal130290 <30610298+ankitsinghal130290@users.noreply.github.com>
2017-08-08 15:26:14 -0700

    Making script generic

--- Scripts/runkat.sh
+++ Scripts/runkat.sh
@@ -4,4 +4,4 @@ export JAVA_HOME=/opt/java/jdk1.8.0
 export PATH=$PATH:$JAVA_HOME/bin
 
 exec java -classpath /usr/bin/sphn/kat-service/kafka-availability-monitor/kafkaavailability-1.0-SNAPSHOT-jar-with-dependencies.jar \
-            com.microsoft.kafkaavailability.App -c van-03-siphon
+            com.microsoft.kafkaavailability.App
